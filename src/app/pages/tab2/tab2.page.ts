0b3fc474794138f3ec7c76ba5d74cfa5e888741f
Nora <nsansaturnino@birt.eus>
2022-12-08 14:30:38 +0100

    Guardar cambios

---
+++ src/app/pages/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import { GestionNoticiasLeerService } from './../../services/gestion-noticias-leer.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(public gestionNoticiasLeer: GestionNoticiasLeerService) {
+
+  }  
+
+}
